729c6e7fd123d3bdcc9624a2625db79fd7eda85f
Itamar Weiss <weiss.itamar@gmail.com>
2014-09-02 21:50:09 +0200

    Created directives for bar and pie

--- angular-chartist.js
+++ angular-chartist.js
@@ -1,53 +1,99 @@
-var angularChartist = angular.module('angular-chartist', []);
+(function(){
+  var angularChartist = angular.module('angular-chartist', []);
 
-angularChartist.directive('chartist', function() {
-  var getSelectorFromElm = function(elm) {
-    var classNames, id, selector;
-    selector = elm.parents().map(function() {
-      return this.tagName;
-    }).get().reverse().join(" ");
-    if (selector) {
-      selector += " " + elm[0].nodeName;
-      id = elm.attr("id");
-      if (id) {
-        selector += "#" + id;
-      }
-      classNames = elm.attr("class");
-      if (classNames) {
-        selector += "." + $.trim(classNames).replace(/\s/gi, ".");
-      }
-    }
-    return selector;
-  };
+  angularChartist.directive('chartistLine', function() {
+    var linkFn = function(scope, elm, attrs) {
+      var data, options, responsiveOptions, selector, updateChart;
+      data = scope.data;
+      options = scope.options;
+      responsiveOptions = scope.responsiveOptions;
 
-  var linkFn = function(scope, elm, attrs) {
-    var data, options, responsiveOptions, selector, updateChart;
-    data = scope.data;
-    options = scope.options;
-    responsiveOptions = scope.responsiveOptions;
-    selector = getSelectorFromElm(elm);
+      updateChart = function(){
+        Chartist.Line(elm.context, data, options, responsiveOptions);
+      };
 
-    updateChart = function(){
-      Chartist.Line(selector, data, options, responsiveOptions);
+      scope.$watch('data', function(newValue, oldValue){
+        data = newValue;
+        updateChart();
+      });
+
+      updateChart();
     };
 
-    scope.$watch('data', function(newValue, oldValue){
-      data = newValue;
+    return {
+      restrict: 'EA',
+      template: '<div class="ct-chart"></div>',
+      replace: true,
+      scope: {
+        data: '=',
+        options: '=',
+        responsiveOptions: '='
+      },
+      link: linkFn
+    };
+  });
+
+  angularChartist.directive('chartistPie', function() {
+    var linkFn = function(scope, elm, attrs) {
+      var data, options, responsiveOptions, selector, updateChart;
+      data = scope.data;
+      options = scope.options;
+      responsiveOptions = scope.responsiveOptions;
+
+      updateChart = function(){
+        Chartist.Pie(elm.context, data, options, responsiveOptions);
+      };
+
+      scope.$watch('data', function(newValue, oldValue){
+        data = newValue;
+        updateChart();
+      });
+
       updateChart();
-    });
+    };
 
-    updateChart();
-  };
+    return {
+      restrict: 'EA',
+      template: '<div class="ct-chart"></div>',
+      replace: true,
+      scope: {
+        data: '=',
+        options: '=',
+        responsiveOptions: '='
+      },
+      link: linkFn
+    };
+  });
 
-  return {
-    restrict: 'E',
-    template: '<div class="ct-chart"></div>',
-    replace: true,
-    scope: {
-      data: '=',
-      options: '=',
-      responsiveOptions: '='
-    },
-    link: linkFn
-  };
-});
+  angularChartist.directive('chartistBar', function() {
+    var linkFn = function(scope, elm, attrs) {
+      var data, options, responsiveOptions, selector, updateChart;
+      data = scope.data;
+      options = scope.options;
+      responsiveOptions = scope.responsiveOptions;
+
+      updateChart = function(){
+        Chartist.Bar(elm.context, data, options, responsiveOptions);
+      };
+
+      scope.$watch('data', function(newValue, oldValue){
+        data = newValue;
+        updateChart();
+      });
+
+      updateChart();
+    };
+
+    return {
+      restrict: 'EA',
+      template: '<div class="ct-chart"></div>',
+      replace: true,
+      scope: {
+        data: '=',
+        options: '=',
+        responsiveOptions: '='
+      },
+      link: linkFn
+    };
+  });
+})();
